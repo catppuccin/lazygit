5c7394a25a8a2ceeb8546c97d8ccd52e65b7847c
nullishamy <git@amyerskine.me>
2023-12-17 16:55:04 +0000

    feat: use whiskers; make file naming consistent

--- flake.nix
+++ flake.nix
@@ -2,16 +2,18 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     flake-utils.url = "github:numtide/flake-utils";
+    catppuccin-toolbox.url = "github:catppuccin/toolbox";
   };
 
   description = "Development shell flake";
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: {
+  outputs = { self, nixpkgs, flake-utils, catppuccin-toolbox }: flake-utils.lib.eachDefaultSystem (system: {
     devShell = let 
       pkgs = nixpkgs.legacyPackages.${system};
     in 
       pkgs.mkShell {
         packages = with pkgs; [
-          deno
+          just
+          catppuccin-toolbox.packages.${system}.whiskers
         ];
       };
   });
